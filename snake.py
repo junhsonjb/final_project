be690fd98834ab03e9e0c8d5500f801eeb14411a
Junhson Jean-Baptiste <junhsonjb@Junhsons-MacBook-Pro.local>
2017-04-25 18:50:03 -0400

    fixed snake model to a tested and working form

--- snake.py
+++ snake.py
@@ -1,65 +1,41 @@
+#import statements
 import random
 import square
 
+#class definition
 class Snake:
-	def __init__(self, boardSize, length=1, color=1):
-		#CONSIDER REMOVING THE length VARIABLE
-		#IF YOU DO, YOU COULD SEE LENGTH BY CHECKING len(self.bodycoords)
-		self.length = length
-		self.color = color
+	#initalizer function
+	def __init__(self, boardSize):
+		self.blockSize = 10 #the size of each block to be built, must be same as apple block size
+		self.boardSize = boardSize #the width and height dimensions of the board
+		self.snakehead = [] #list to be cleared in game loop, holds snake head coordinates
+		self.snakelist = [] #list to keep all snake body coordinates
+		self.snakelength = 1 #length to allow snake to grow to, MUST start at one
+		self.xchange = 0 #amount to change x coord by, constantly added to xcord in game loop
+        self.ychange = 0 #amount to change y coord by, constantly added to ycord in game loop
+        self.xcord = boardSize[0] / 2 #x coordinate of snake body part, start in center of board
+        self.ycord = boardSize[1] / 2 #y coordinate of snake body part, start in center of board
 
-		self.boardSize = boardSize#should be a tuple with board's x and y coords
-		#self.xCor = ( boardSize[0] ) / 2
-		#self.yCor = ( boardSize[1] ) / 2
-		#Initialize the first body part coordinate in the center of screen
-		self.bodycoords = [ [(self.boardSize[0] / 2), (self.boardSize[1] / 2)] ]
+	#the function to move up on the board
+	def up(self):
+		self.ychange = -self.blockSize
+		self.xchange = 0
 
-	# def getXCor(self):
-	# 	return self.xCor
+	#the function to move down on the board
+	def down(self):
+		self.ychange = self.blockSize
+		self.xchange = 0
 
-	# def getYCor(self):
-	# 	return self.yCor
+	#the function to move left on the board
+	def left(self):
+		self.xchange = -self.blockSize
+		self.ychange = 0
 
-	def getbodycoords(self):
-		return self.bodycoords
+	#the function to move right on the board
+	def right(self):
+		self.xchange = self.blockSize
+		self.ychange = 0
 
-	def getLength(self):
-		return self.length
-
-	#RETURN BOOLEAN VALUE BASED ON IF SNAKE PARTS ARE IN BOUNDS OF BOARD
-	def inBounds(self):
-		for i in range(len(self.bodycoords)):
-			#IF: the x and y coords of each body part is within the x and y coords of the board:
-			if (self.bodycoords[i][0] < self.boardSize[0] and self.bodycoords[i][1] < self.boardSize[1]):
-				return True
-			else:
-				return False
-
-	#MOVEMENT FUNCTIONS
-	def moveRight(self):
-		while ( inBounds() ):
-			for i in range(len(self.bodycoords)):
-				self.bodycoords[i][0] += 1
-
-	def moveLeft(self):
-		while ( inBounds() ):
-			for i in range(len(self.bodycoords)):
-				self.bodycoords[i][0] -= 1
-
-	def moveUp(self):
-		while ( inBounds() ):
-			for i in range(len(self.bodycoords)):
-				self.bodycoords[i][1] -= 1
-
-	def moveDown(self):
-		while ( inBounds() ):
-			for i in range(len(self.bodycoords)):
-				self.bodycoords[i][1] += 1
-
-	#GROWTH FUNCTION
-	def grow(self):
-		#add another list to bodycoords, where x and y are += 1 of its previos list element
-		self.bodycoords += [(self.bodycoords[-1][0] + 1), (self.bodycoords[-1][1] + 1)]
-
+	#magic string function
 	def __str__(self):
-		return "head x coorindate: " + str(self.bodycoords[0][0]) + "\nbody y coordinate: " + str(self.bodycoords[0][1])
+		return "Snake length: " + str(self.snakelength) + ", snake x coordinate: " + str(self.xcord) + ", snake y coordinates: " + str(self.ycord) + "."
